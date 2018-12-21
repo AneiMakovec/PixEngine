8b077baf7dd522e3a2a31138c40828fda4c7d3f1
AneiMakovec <anei.makovec@hotmail.com>
2018-12-21 04:09:24 +0100

    Organized class Level and added new class Dicepool.

---
+++ PixEngine/Graphics/Primitive/PixEngine.Graphics.Primitive.classes.h
@@ -0,0 +1,9 @@
+//
+//  PixEngine.Graphics.Primitive.classes.h
+//  PixEngine
+//
+//  Created by Anei Makovec on 21/12/2018.
+//  Copyright © 2018 Anei Makovec. All rights reserved.
+//
+
+@class PrimitiveBatch;
---
+++ PixEngine/Graphics/Primitive/PixEngine.Graphics.Primitive.h
@@ -0,0 +1,9 @@
+//
+//  PixEngine.Graphics.Primitive.h
+//  PixEngine
+//
+//  Created by Anei Makovec on 21/12/2018.
+//  Copyright © 2018 Anei Makovec. All rights reserved.
+//
+
+#import "PrimitiveBatch.h"
